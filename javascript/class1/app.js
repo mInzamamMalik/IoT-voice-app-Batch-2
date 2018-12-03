562b50b5f8e9efb9140f0b8cf717c985c7126cf2
Inzamam Malik <malikasinger1@users.noreply.github.com>
2018-12-03 16:39:30 +0500

    Add files via upload

---
+++ javascript/class1/app.js
@@ -0,0 +1,84 @@
+
+// let name = "Mark";
+// console.log(name);
+
+
+// name = "Ace";
+// console.log(name);
+
+
+// var nationality;
+
+// nationality = "U.S.";
+
+
+// var floogle = "Mark";
+// var name = "Mark"
+// var lessonAuthor = "Mark";
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+// var x = "Mark";
+
+
+// var nickname = "Bub";
+
+
+// console.log("Thanks for your input!");
+
+
+// var thanx = "Thanks for your input!"
+// console.log(thanx);
+
+
+// var weight = 150;
+
+// weight + 25
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+
+// console.log(newNum)
+
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum)
+
+
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+// console.log(originalNum);
+
+
+// var originalNumber = 23;
+// var newNumber = originalNumber + 7;
+
+// console.log(newNumber);
+
+
+
+// var 1stPresident = "Washington"; // illigal
+
+var prezWhoCame1st = "Washington";
+
+
+
+console.log(144);
+
+
+var caseQty = 144;
+console.log(caseQty);
+
+
+
+
+
+
+
+
+
+
+
+
+
