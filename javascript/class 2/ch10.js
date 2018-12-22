015b37a62b272212044af52fbc4d8d5b34fba774
Anjum Ali <anjumalidsd@gmail.com>
2018-12-22 20:35:37 +0500

    Batch2, Class2, Class work.

---
+++ javascript/class 2/ch10.js
@@ -0,0 +1,7 @@
+var lucckyDrawResult = 7;
+var userInput = prompt("Enter number : ");
+
+if (userInput == lucckyDrawResult)
+    alert("You Win");
+else
+    alert("You Loss");
---
+++ javascript/class 2/ch6.js
@@ -0,0 +1,13 @@
+var num1 = 5;// 
+var num2 = 8 //9,8,9
+var num3 = 0;
+var num4 = 0;
+
+//num3 = num1++ + --num1; // 5 + 5
+//console.log(num3);
+
+num4 = ++num2- --num2 + num2++ + num1-- -num2;
+console.log(num4);
+// 6
+//9 - 8 + 8 + 5 - 9
+// 5
